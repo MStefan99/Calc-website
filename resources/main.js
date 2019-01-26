a93c7ec6f4db013a19e3477e086821ad85dcba6b
Mikhail Stefantsev <stephan05.08.67@gmail.com>
2019-01-26 20:34:21 +0200

    added text animation

--- resources/main.js
+++ resources/main.js
@@ -1,6 +1,12 @@
+
 let wrapper = document.getElementById('loader-wrapper');
 let loader = document.getElementById('loader');
 let aboutPopup = document.getElementById('aboutPopup');
+var line1 = 'Annushka Project\'s';
+var line2 = 'Calculator';
+var line3 = 'The last calculator you\'ll ever need';
+charTime = 70;
+var i = 0;
 
 time = setTimeout(load, 1000);
 
@@ -22,4 +28,38 @@ window.onload = function() {
     wrapper.classList.add('loaded');
     loader.classList.add('loaded');
     clearTimeout(time);
+    typeWriter1();
+};
+
+function typeWriter1() {
+    if (i < line1.length) {
+        document.getElementById("line1").innerHTML += line1.charAt(i);
+        i++;
+        setTimeout(typeWriter1, charTime + 10);
+    } else {
+        i = 0;
+        typeWriter2();
+    }
+}
+
+function typeWriter2() {
+    if (i < line2.length) {
+        document.getElementById("line2").innerHTML += line2.charAt(i);
+        i++;
+        setTimeout(typeWriter2, charTime + 30);
+    } else {
+        i = 0;
+        typeWriter3();
+    }
+
+}
+
+function typeWriter3() {
+    if (i < line3.length) {
+        document.getElementById("line3").innerHTML += line3.charAt(i);
+        i++;
+        setTimeout(typeWriter3, charTime);
+    } else {
+
+    }
 }
